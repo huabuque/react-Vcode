1052a9bbbf0c969be98ace891d9901fb9f31040c
zzzzhaoziyi <github@sunlei.mobi>
2018-05-13 01:47:23 +0800

    update by sl

--- VCode.js
+++ VCode.js
@@ -33,15 +33,30 @@ class VCode extends Component {
   }
 
   canvas() {
-    
+    const { getRandom } = this
+    const canvas = document.getElementById('bgi')
+    let ctx = canvas.getContext('2d')
+    canvas.height = canvas.height
+    // ctx.clearRect(0, 0, canvas.width(), canvas.height())
+    ctx.strokeStyle = `rgb(${this.getRandom(100,10,3).toString()})`
+    for( let i = 0; i< 7; i++ ) {
+      ctx.lineTo(getRandom(200,0),getRandom(200,10))
+      ctx.moveTo(getRandom(200,0),getRandom(200,0))
+      ctx.stroke();
+    }
+  }
+  componentDidMount() {
+    this.canvas()
   }
 
   render() {
     const { rotate, fz, color } = this.state
     return (
       <div className='vcodewrap' >
+        <canvas id="bgi" width="200" height="200"></canvas>
         {this.state.data.map((v,i) => 
           <div 
+            key={i}
             className='itemStr'
             style={{
               transform:`rotate(${rotate[i]}deg)`,
@@ -53,11 +68,15 @@ class VCode extends Component {
             {String.fromCharCode(v > 57 && v < 84 ? v + 7 : ( v < 57 ? v : v + 13 ))}
           </div>  
         )}
-      {this.state.refresh
+      {
+        this.state.refresh
         ? <div 
             className='mask'
-            onClick={() => this.setState({...this.initState(),refresh: false})}
-            onMouseLeave={() => this.setState({refresh: false})}
+            onClick={() => {
+              this.setState({...this.initState(),refresh: false})
+              this.canvas()
+            }}
+            onMouseLeave={() => {this.setState({refresh: false})}}
           > 看不清？点击刷新  
           </div> 
         : null}
