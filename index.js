41a2683e02c867eabe63486b9f481dd44fc67b06
Lei Sun <github@sunlei.mobi>
2018-07-19 13:57:33 +0800

    delete

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import VCode from './VCode';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<VCode />, document.getElementById('root'));
-registerServiceWorker();
